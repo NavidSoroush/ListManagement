bb65cdad1cab4900d5a6311161225bf8b0e664b6
rickyschoolsFS <ricky.schools@franklinsquare.com>
2016-06-28 11:51:58 -0400

    Finra License Search - 2 attempts to find license
    Thanks to max for providing a way that the code now tries twice on
    'problem' CRD numbers.

--- finra_licenses.py
+++ finra_licenses.py
@@ -12,40 +12,55 @@ finra_site = 'http://brokercheck.finra.org/Individual/Summary/'
 chromedriver = "C:/Python27/selenium/Chrome/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 elements=['col-md-3']
-##path='C:/Users/rschools/Desktop/CRD_Test.xlsx'
+path='C:/Users/rschools/Desktop/CRD_Test.xlsx'
 
-def licenseSearch(path):
+def licenseSearch(path, attempted_search_count=0,attempts=0):
     df=pd.read_excel(path)
     df['Licenses']=''
     print 'Pulling licenses from FINRA for %s advisors.' % len(df[df['CRDNumber'].notnull()].index)
     sel = webdriver.Chrome(chromedriver)
     wait = WebDriverWait(sel, 1)
-    for index, row in df.iterrows():
-        if row['CRDNumber']!='':
-            crd=row['CRDNumber']
-            try:
-                url=finra_site+str(crd)
-                lic=[]
-                sel.get(url)
-                
-                license_code=wait.until(EC.visibility_of_element_located((By.CLASS_NAME,elements[0])))
-                reg_info=sel.find_elements_by_class_name(elements[0])
-                for reg in reg_info:
-                    if reg.text[:6]=='Series':
-                        lic.append(int(reg.text[7:]))
-                lic.sort()
-                lic=['Series {0}'.format(l) for l in lic]
-            except Exception,e:
-                lic=[]
-                print Exception, e
+    while attempted_search_count < len(df['CRDNumber']):
+        if df['CRDNumber'][attempted_search_count]!='':
+            if attempts < 2:
+                try:
+                    crd=df['CRDNumber'][attempted_search_count]
+
+                    url=finra_site+str(crd)
+                    sel.get(url)
+                    lic=[]
+                    
+                    license_code=wait.until(EC.visibility_of_element_located((By.CLASS_NAME,elements[0])))
+                    reg_info=sel.find_elements_by_class_name(elements[0])
+                    for reg in reg_info:
+                        if reg.text[:6]=='Series':
+                            lic.append(int(reg.text[7:]))
+                    lic.sort()
+                    lic=['Series {0}'.format(l) for l in lic]
+                    df.loc[attempted_search_count,['Licenses']]=';'.join(lic)
+                    attempted_search_count+=1
+                    del lic
+                except:
+                    lic=[]
+                    attempts+=1
+##                    print 'Refresh #%s for %s' % (attempts, crd)
+            else:
+                attempts = 0
+                lic=['CRD Not found']
+                df.loc[attempted_search_count,['Licenses']]=';'.join(lic)
+                attempted_search_count+=1
+                del lic
         else:
             lic=[]
-        df.loc[index,['Licenses']]=';'.join(lic)
-        del lic
+            df.loc[attempted_search_count,['Licenses']]=';'.join(lic)
+            attempted_search_count+=1
+            del lic
+
     sel.close(), sel.quit()
     df.to_excel(path, index=False)
     return {'BDG Finra Scrape':'Success'}
-##licenseSearch(path)
+
+licenseSearch(path)
 
         
 
