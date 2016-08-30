02293bb4855e8c271ca1fe04c5d3dedc6e8404a1
rickyschoolsFS <ricky.schools@franklinsquare.com>
2016-08-30 11:46:58 -0400

    Search: Included untested method 'sec_search'
    This method, like the others, needs to be tested prior to itegrating
    into the production version of the list program.

--- Search.py
+++ Search.py
@@ -50,7 +50,7 @@ def sf_advlist():
 user = os.environ.get("USERNAME")
 AdvListPath = 'T:/Shared/FS2 Business Operations/Search Program/Salesforce Data Files/SFDC Advisor List as of ' \
               + time.strftime("%m-%d-%y") + '.csv'
-if os.path.exists(AdvListPath) == False:
+if not os.path.exists(AdvListPath):
     sf_advlist()
 
 
@@ -59,7 +59,7 @@ def df_column_preprocessing(df):
     for col in df.columns:
         if df[col].dtype not in ('int64', 'float64', 'object'):
             # print count, df[col].dtype, df[col]
-            for cell in xrange(0, len(df[col])):
+            for cell in range(0, len(df[col])):
                 df[cell][count] = df[cell][count].encode('utf-8', 'ignore')
         else:
             count += 1
@@ -80,6 +80,10 @@ def init_advisor_list():
 
 def read_list(path_to_load):
     return pd.read_excel(path_to_load, sheetname=0)
+
+
+def read_csv(path_to_load):
+    return pd.read_csv(path_to_load)
 
 
 def name_preprocessing(headers, search_list):
@@ -155,6 +159,8 @@ def lkup_name_address_processing(headers, search_list):
 
 class Search():
     def __init__(self):
+        import datetime
+        self._today = datetime.datetime.strftime(datetime.datetime.now(), '%m_%d_%Y')
         self._SFDC_advisor_list = init_advisor_list()
         self._search_fields = ['AMPFMBRID', 'Email', 'LkupName']
         self._return_fields = ['AccountId', 'SourceChannel', 'Needs Info Updated?',
@@ -167,6 +173,8 @@ class Search():
         self._num_found_contacts = 0
         self._review_path = ''
         self._found_contact_path = ''
+        self._sec_files = ''
+        self._sec_path = 'T:/Shared/FS2 Business Operations/Python Search Program/SEC_Data/Individuals/processed_data/' + self._today + '/'
 
     def __init_list_metadata(self):
         self._headers = self._search_list.columns.values
@@ -268,7 +276,7 @@ class Search():
         if 'CRDNumber' in self._headers:
             self._crd_search()
 
-        if self._to_finra == False:
+        if not self._to_finra:
             print('CRD Info provided for all contacts. Will not search FINRA.')
 
         else:
@@ -312,73 +320,30 @@ class Search():
                     'Found in SFDC Search #2': self._num_found_contacts}
         return ret_item
 
+    def perform_sec_search(self, searching_list_path, found_path):
+        if not os.path.exists(self._sec_path):
+            print("Skipping SEC Search. Today's files could not be found in the listed directory.")
+            ret_item = {'Next Step': 'SFDC Search #2', 'SEC_Found': 0}
+            return ret_item
 
-def searchsec(path, found_path):
-    '''
-    Following the 'to-be-searched' list that had already be searched against
-    SFDC, and attempted FINRA scraping, the remaining names that could not be
-    scrapped are searched against the SEC database for CRD scrapping.
-    database
+        print('\nStep 6:\nSearching against SEC Data.')
+        self._search_list = read_list(searching_list_path)
+        self.__init_list_metadata()
+        self._sec_files = os.listdir(self._sec_path)
+        self._search_fields = ['LkupName']
 
-    After the scrapping, we re-search the SFDC database to attempt to find any
-    additional contacts that were missed during the 'searchone' function.
-    '''
+        for sec in self._sec_files:
+            self._sec_df = read_csv(sec)
+            self._sec_df.rename(columns={'indvlPK': 'CRDNumber'}, inplace=True)
+            self.__search_and_merge(self._search_fields)
 
-    import datetime
-    today = datetime.datetime.strftime(datetime.datetime.now(), '%m_%d_%Y')
-    SECpath = 'T:/Shared/FS2 Business Operations/Python Search Program/SEC_Data/Individuals/processed_data/' + today + '/'
-    if os.path.exists(SECpath) == False:
-        print("Skipping SEC Search. Today's files could not be found in the listed directory.")
-        ret_item = {'Next Step': 'SFDC Search #2', 'SEC_Found': 0}
+        self._found_contacts.to_excel(found_path, index=False)
+        self._search_list.to_excel(searching_list_path, index=False)
+
+        ret_item = {'Next Step': 'SFDC Search #2', 'SEC_Found': self._num_found_contacts}
         return ret_item
 
-    print('\nStep 6:\nSearching against SEC Data.')
-    Campaign_list = pd.read_excel(path, sheetname=0)
-    headers = Campaign_list.columns.values
-    # test_SECpath='C:/Users/rschools/Downloads/SEC_Data/processed_data/03_30_2016/'
-    SECfiles = os.listdir(SECpath)
-    found = 0
-    for sec in SECfiles:
-
-        sec_df = pd.read_csv(SECpath + sec)
-        sec_df.rename(columns={'indvlPK': 'CRDNumber'}, inplace=True)
-
-        searchfields = ['LkupName']
-        found_contacts = pd.DataFrame()
-
-        n = 0
-        for header in searchfields:
-            if header in headers:
-                n += 1
-
-                print('Performing search #%s on %s for %s sec file' % (n, header, sec))
-                headerandIDs = sec_df[[header, 'CRDNumber']]
-                Campaign_list = Campaign_list.merge(headerandIDs, how='left', on=header)
-                Campaign_list = Campaign_list.fillna('')
-                found_contacts = found_contacts.append(Campaign_list[Campaign_list['CRDNumber'] != ''],
-                                                       ignore_index=True)
-                Campaign_list = Campaign_list[Campaign_list.CRDNumber == '']
-                if n > 1:
-                    found = (len(found_contacts) - found)
-                else:
-                    found = len(found_contacts)
-                print('Found %s on %s search.' % (found, header))
-                del Campaign_list['CRDNumber']
-
-    df = pd.read_excel(found_path, sheetname=0)
-    df = df.append(found_contacts)
-    df.to_excel(found_path, index=False)
-    Campaign_list.to_excel(path, index=False)
-    ret_item = {'Next Step': 'SFDC Search #2', 'SEC_Found': found}
-    return ret_item
-
-
-##test_nocrd='C:/Users/rschools/Dropbox/Python Search Program/New Lists/Chicago Pre Attendee_nocrd.xlsx'
-##test_found='C:/Users/rschools/Dropbox/Python Search Program/New Lists/Chicago Pre Attendee_foundcontacts.xlsx'
-##
-
-if __name__ == "__main__":
-    test_search1 = 'C:/Users/rschools/Desktop/search_class_test.xlsx'
-
-    search = Search()
-    print(search.perform_search_one(test_search1, list_type='Account'))
+# if __name__ == "__main__":
+#     test_search1 = 'C:/Users/rschools/Desktop/search_class_test.xlsx'
+#     search = Search()
+#     print(search.perform_search_one(test_search1, list_type='Account'))
