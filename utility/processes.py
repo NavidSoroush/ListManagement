1daf454dda142facad64b2042e9e3d44de3ad52c
rickyschoolsFS <ricky.schools@franklinsquare.com>
2017-04-06 10:06:28 -0400

    adding the ability to identify items that need manual preprocessing

--- utility/processes.py
+++ utility/processes.py
@@ -313,7 +313,7 @@ def bdg_upload(session, data, obj_id, obj, col_num, df_path, remove_path=None, a
     sf_bdg_members = session.get_current_members(obj_id=obj_id, obj=obj)
     to_insert, to_update, to_remove = split_list(sf_bdg_members, data, obj_id, obj, col_num[1])
     curr_memb = create_path_name(df_path, 'current_bdg_members')
-    sf_bdg_members = [sf_bdg_members[i:i+2] for i in range(0, len(sf_bdg_members), 2)]
+    sf_bdg_members = [sf_bdg_members[i:i + 2] for i in range(0, len(sf_bdg_members), 2)]
     save_df(df=make_df(data=sf_bdg_members, columns=['Id', 'BizDev_Group__c']), path=curr_memb)
     print('Attempting to associate %s to the BizDev Group.' % len(to_insert))
     if len(to_insert) > 0:
@@ -339,3 +339,37 @@ def bdg_upload(session, data, obj_id, obj, col_num, df_path, remove_path=None, a
     session.last_list_uploaded(obj_id=obj_id, obj=obj)
 
     return [remove_path, add_path, update_path, curr_memb], [n_re, n_add, n_up]
+
+
+def id_preprocessing_needs(path):
+    # should be comprised of three steps.
+    # 1. Evaluate path extension type.
+    # 2. If file is of appropriate type - check if:
+        # a. top row is blank
+        # b. remove any random blank rows
+        # c. identify if there is a bad header row
+    # 3. Notify user of needs / actions performed
+
+    _accepted_file_ext = ['.xlsx', '.xls', '.csv']
+    has_bad_file_ext = False
+    needs_manual_intervention = False
+    has_malformed_header_row = False
+    has_blank_rows = False
+
+    extension = os.path.splitext(path)[1]
+    if extension not in _accepted_file_ext:
+        has_bad_file_ext = True
+
+    if not has_bad_file_ext:
+        df = read_df(path)
+
+        if df.shape[0] - df.dropna().shape[0] > 0:
+            has_blank_rows = True
+
+    if has_bad_file_ext or has_malformed_header_row or has_blank_rows:
+        needs_manual_intervention = True
+
+    print({'needs_intervention': needs_manual_intervention,
+           'bad_ext': has_bad_file_ext,
+           'blank_rows': has_blank_rows,
+           'malformed_header': has_malformed_header_row})
