e4e0343dc9a2cd5af1500c41a7ef63c75df1baa8
rickyschoolsFS <ricky.schools@franklinsquare.com>
2017-05-17 12:50:32 -0400

    attempt to build a replacement for VBA email handler / forwarding
    fixes #95
    
    additional issues (or tasks) should be create to enhance and refine the
    email handler.

--- requested_email_job.py
+++ requested_email_job.py
@@ -1,4 +1,7 @@
 import email
+from email.parser import Parser
+import os
+import shutil
 import datetime
 from dateutil.parser import parse
 import imaplib
@@ -6,15 +9,18 @@ import sys
 from cred import outlook_userEmail, password
 import pandas as pd
 import sqlalchemy
-from progress_bar import myprogressbar
+from utility.progress_bar import myprogressbar
 from email_handler.email_wrapper import Email
 
 _acceptable_types = ['lsx', 'pdf', 'csv', 'xls', 'zip', 'ocx', 'txt']
 _email_account = outlook_userEmail + '/Lists'
-_email_folder = [['INBOX/New Lists/', 'ReceivedLists'],
-                 ['INBOX/Auto Processed Lists/', 'ProcessedLists'],
-                 ['INBOX/Salesforce Contact Creation Logs/', 'ContactCreationResponses']
-                 ]
+_email_folder = [
+    ['INBOX/', 'Inbox'],
+    ['INBOX/New Lists/', 'ReceivedLists'],
+    ['INBOX/Auto Processed Lists/', 'ProcessedLists'],
+    ['INBOX/Salesforce Contact Creation Logs/', 'ContactCreationResponses']
+]
+_temp_save_attachments = 'C:/save_att/'
 
 
 def get_msg_part(msg_part, array):
@@ -34,6 +40,20 @@ def get_msg_part(msg_part, array):
     if msg_part == 'date':
         tmp = date_to_string(tmp)
     return tmp
+
+
+def handle_email_attachments(remove=False, msg_part=None, att=None):
+    if remove:
+        if os.path.isdir(_temp_save_attachments):
+            shutil.rmtree(_temp_save_attachments)
+    else:
+        if not os.path.isdir(_temp_save_attachments):
+            os.mkdir(_temp_save_attachments)
+        if att is not None:
+            new_fname = _temp_save_attachments + att
+            with file(_temp_save_attachments + att, mode='w') as f:
+                f.write(msg_part.get_payload(decode=True))
+            return new_fname
 
 
 def email_parser(sender_name, look1, look2=None):
@@ -75,6 +95,12 @@ def date_to_string(d_value):
     return datetime.datetime.strftime(d, '%m/%d/%Y %H:%M:%S')
 
 
+def move_received_list_to_processed_folder(mailbox, num):
+    mailbox.copy(num, 'INBOX/New Lists')
+    mailbox.store(num, '+FLAGS', r'(\Deleted)')
+    mailbox.expunge()
+
+
 m = imaplib.IMAP4_SSL('outlook.office365.com')
 try:
     rv, data = m.login(_email_account, password)
@@ -108,24 +134,43 @@ for ef in _email_folder:
                     tmp_dict['SenderName'] = email_parser(tmp_dict['SenderEmail'], ' <')
                     tmp_dict['SenderEmail'] = email_parser(tmp_dict['SenderEmail'], '<', '>')
                     e_item = email.message_from_string(data[0][1])
+                    msg_body = "Request from %s for %s list.\n\n" \
+                               "Please look in the 'New Lists' folder of the list inbox " \
+                               "for more details." % (tmp_dict['SenderName'],
+                                                      tmp_dict['Subject'])
                     attachments = []
                     for part in e_item.walk():
-                        file_name = None
+                        if part.get_content_type() == "text/plain":
+                            msg_body += part.get_payload(decode=True)
                         if part.get_content_maintype() == 'multipart': continue
                         if part.get('Content-Disposition') is None: continue
-                        attachments.append(part.get_filename())
+                        file_name = part.get_filename()
+                        if file_name is not None:
+                            file_name = handle_email_attachments(msg_part=part, att=file_name)
+                            attachments.append(file_name)
                     if len(attachments) > 0:
                         attachments = [a for a in attachments if a[-3:].lower() in _acceptable_types]
                         for a in range(0, len(attachments)):
                             k = 'AttachmentName' + str(a + 1)
                             tmp_dict[k] = attachments[a]
                     _elements.append(tmp_dict)
+                    if ef[0] == 'INBOX/':
+                        email_to = ['rickyschools+v3lhm65etri76gwbn0sy@boards.trello.com',
+                                    'max.charles@fsinvestments.com',
+                                    'ricky.schools@fsinvestments.com']
+                        sub = 'New List Received -' + tmp_dict['Subject']
+                        e_item.replace_header('From', 'lists@fsinvestments.com')
+                        e_item.replace_header('To', email_to)
+                        e_item.replace_header('Subject', sub)
+                        Email(subject=sub, to=email_to, body=msg_body, attachment_path=attachments)
+                        move_received_list_to_processed_folder(mailbox=m, num=i)
+                        handle_email_attachments(remove=True)
                 del e_item
                 del tmp_dict
-                del attachments
                 count += 1
                 m.store(i, '+FLAGS', r'(\SEEN)')
-    if num_emails > 0:
+
+    if num_emails > 0 and ef[0] != "INBOX/":
         engine = sqlalchemy.create_engine('mssql+pyodbc://DPHL-PROPSCORE/ListManagement?driver=SQL+Server')
         notification = "\nWriting %s records to the '%s' table in the ListManagement DB." % (num_emails, ef[1])
         print(notification)
