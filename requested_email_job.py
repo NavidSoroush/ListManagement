072f73b2f837ff6af922c580159a6621fc4f101d
rickyschoolsFS <ricky.schools@franklinsquare.com>
2017-05-19 12:56:55 -0400

    Revert "adding error handler for list requests and formatting pushes"
    This reverts commit 460b2b10cfc1df1739d3f6dc5b80ddd5cd5f7cf3.

--- requested_email_job.py
+++ requested_email_job.py
@@ -1,12 +1,7 @@
 from utility.email_reader import MailBoxReader
 from utility.log_helper import ListManagementLogger
-from utility.email_wrapper import Email
-import traceback
+
 
 log = ListManagementLogger().logger
 mb = MailBoxReader(log=log)
-try:
-    mb.extract_pending_lists(mailbox=mb.mailbox, folder=mb.new_requests_folder)
-except:
-    sub = 'LMA: Requested Email Notification - Unable to Process'
-    Email(subject=sub, to=['ricky.schools@fsinvestments.com'], body=str(traceback.format_exc()), attachment_path=None)
+mb.extract_pending_lists(mailbox=mb.mailbox, folder=mb.new_requests_folder)
