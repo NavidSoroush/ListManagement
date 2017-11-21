108b6a83d61ae4b678c28469be838f3811ddea2a
rickyschoolsFS <ricky.schools@fsinvestments.com>
2017-11-21 14:05:20 -0500

    commenting sample code

--- tests_and_samples/sample_code/sample_ML_interactions.py
+++ tests_and_samples/sample_code/sample_ML_interactions.py
@@ -3,10 +3,13 @@ from ListManagement import HeaderPredictions, ListManagementLogger
 log = ListManagementLogger().logger
 
 # to run only diagnostics to automatically find the best performing algorithm
+# (Recommended use: weekly shell or task script)
 hp = HeaderPredictions(log=log, run_diagnostics='only_diagnostics')
 
-# to run diagnostics to automatically find the best performing algorithm and leverage it on the fly
-hp = HeaderPredictions(log=log, run_diagnostics='True', use_saved=True)
+# to run diagnostics to automatically find the best performing algorithm
+# and leverage it in a production run, on the fly. (Not recommended)
+hp = HeaderPredictions(log=log, run_diagnostics=True, use_saved=True)
 
 # to leverage the best, pre-selected, algorithm selected based on cross-validation.
+# Recommended use in production code-base
 hp = HeaderPredictions(log=log, use_saved=True)
