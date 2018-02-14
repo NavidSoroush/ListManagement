57a5ef1dd265db1a5bdd6a1cb5108fd3ae3bede8
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-02-14 11:56:20 -0500

    addressing legacy issues, LLU by Object, and logger maintenance
    #139
    #137
    #140

--- legacy/get_sf_adv_list.py
+++ legacy/get_sf_adv_list.py
@@ -5,12 +5,12 @@ import sqlalchemy
 try:
     from ListManagement.legacy.sf_adv_query import list_SQL
     from ListManagement.legacy.sf_adv_formatting import make_lookup_name, needs_update_flag
-    from ListManagement.utility import auto_maintain, os, duration, datetime
+    from ListManagement.utility import auto_maintain, os, duration, time
     from ListManagement.utility.pandas_helper import pd
 except:
     from legacy.sf_adv_query import list_SQL
     from legacy.sf_adv_formatting import make_lookup_name, needs_update_flag
-    from utility import auto_maintain, os, duration, datetime
+    from utility import auto_maintain, os, duration, time
     from utility.pandas_helper import pd
 
 
@@ -38,17 +38,20 @@ def run(path_name, logger):
         logger.error('Failed to connect to SQL database.', exc_info=True)
         sys.exit()
 
-    query_start = datetime.datetime.now()
-    logger.info('Preparing the file for use..')
+    query_start = time.time()
+    logger.info(' > Extracting data from SQL.')
     df = pd.read_sql_query(list_SQL, conn)
+    logger.info(' > Preparing the file for use.')
+    logger.debug('   > Making LookupName.')
     df.insert(0, 'LkupName', make_lookup_name(df, lkup_strings))
+    logger.debug('   > Adding update flags.')
     df.insert(len(df.columns), 'Needs info updated?', needs_update_flag(df[df.columns[-3:]], update_cols, 180, 90))
     df.columns = rep_headers
-    logger.info('Saving to FS shared drive.')
+    logger.info(' > Transformation complete. Saving to FS shared drive.')
     df.rename(columns={'CRD': 'CRDNumber'}, inplace=True)
     df = df[~df.CRDNumber.str.contains('[a-zA-Z]').fillna(False)]
     df.to_csv(path_name, header=True, index=False, encoding='utf-8')
-    logger.info('Closing SQL connection....')
-    query_end = datetime.datetime.now()
+    logger.info('Closing SQL connection.')
+    query_end = time.time()
     # printing success and time it took to complete query and save
     logger.info("Saving complete. Query took: %s" % duration(query_start, query_end))
