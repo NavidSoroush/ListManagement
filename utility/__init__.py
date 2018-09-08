ac36d9b19ea6c1918b5a48937ad1c4d2a43795ef
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-09-08 08:50:27 -0400

    completely remove email_wrapper.py

--- utility/__init__.py
+++ utility/__init__.py
@@ -3,4 +3,3 @@ from __future__ import absolute_import
 from .gen_helper import *
 from .log_helper import ListManagementLogger
 from .email_reader import MailBoxReader
-from .email_wrapper import Email
