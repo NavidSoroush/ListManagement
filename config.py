41177db7d5079c7079e9648383adeab29e3f46b9
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-09-09 05:16:09 -0400

    adding more logic/rules to Config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 # import importlib
 # import pip
 # import sys
@@ -35,6 +36,12 @@ import os
 #             print('Unable to install %s. Please paste the below into the command-line.\n%s' %
 #                   (r_name, ' '.join([sys.executable, '-m', 'pip', 'install',
 #                                      '=='.join([r_name, r_version])])))
+_accepted_cols = [
+    'CRDNumber', 'FirstName', 'LastName', 'AccountId'
+    , 'MailingStreet', 'MailingCity', 'MailingState', 'MailingPostalCode'
+    , 'SourceChannel', 'Email', 'Website', 'AUM', 'GDC', 'Fax'
+    , 'HomePhone', 'MobilePhone', 'Phone'
+]
 
 
 class Config(object):
@@ -49,3 +56,12 @@ class Config(object):
     SFToken = os.environ['SFTOKEN']
     SMTPUser = os.environ['SMTP_EMAIL']
     SMTPPass = os.environ['SMTP_PASS']
+    RunningUser = os.environ.get("USERNAME")
+    AcceptedCols = _accepted_cols
+    NecessaryCols = _accepted_cols[:8]
+    BDG_ACCEPTED_COLS = ['ContactID', 'BizDev Group', 'Licenses']
+    CMP_ACCEPTED_COLS = ['ContactID', 'CampaignId', 'Status']
+    NEW_PATH_NAMES = ['_nocrd', '_finrasec_found', '_FINRA_ambiguous',
+                      '_review_contacts', '_foundcontacts', 'cmp_to_create',
+                      'cmp_upload', 'no_updates', 'to_update', 'to_create', 'bdg_update',
+                      'toAdd', 'bdg_toStay', 'current_bdg_members', 'to_remove']
