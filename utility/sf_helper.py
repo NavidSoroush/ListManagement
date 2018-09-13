f5bcdcc4ddca9176f0ad5417b9fab377229a25d4
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-09-13 10:07:09 -0400

    added docstrings for sf_helper.py

--- utility/sf_helper.py
+++ utility/sf_helper.py
@@ -1,9 +1,56 @@
+"""
+sf_helper.py
+====================================
+Contains helper functions that are useful
+when preparing data to upload to Salesforce.
+"""
+
+
 def remove_duplicates(mbr_list):
+    """
+    Helper function to 'uniqify' a list.
+    Parameters
+    ----------
+    mbr_list
+        A list of lists containing duplicates.
+
+    Returns
+    -------
+        A unique set of lists.
+    """
     unique_data = [list(x) for x in set(tuple(x) for x in mbr_list)]
     return unique_data
 
 
 def split_list(id_in_obj, ids_from_search, obj_id, obj, col=None, remove=None, remove_unique=None):
+    """
+    Helper function to take two lists and parse them into 2 to 3 different lists.
+
+    1) If any 'ids_from_search' aren't present in 'id_in_obj', add to insert.
+    2) If any 'ids_from_search' are present in 'id_in_obj', add to update.
+    3) If any 'id_in_obj' aren't present in 'ids_from_search', add to remove.
+        (Only happens for BizDev Group Lists)
+
+    Parameters
+    ----------
+    id_in_obj
+        A list of unique identifiers present in a Salesforce object.
+    ids_from_search
+        A list of unique identifiers present in a 3rd party list.
+    obj_id
+        An 18-char string; Represents an Id of a Salesforce object.
+    obj
+        A string; Represents the name of a Salesforce object.
+    col_num
+        An integer; The column to use for when parsing a list.
+    remove
+        An empty list.
+    remove_unique
+        REMOVE AS NOT USED.
+    Returns
+    -------
+        A tuple of lists (insert, update, remove)
+    """
     if obj == 'Campaign':
         insert = [i for i in ids_from_search if i[0] not in id_in_obj]
         update = [i for i in ids_from_search if i[0] in id_in_obj]
@@ -49,10 +96,41 @@ def cmp_mbr_id_for_contact_id(update_list, obj_list):
 
 
 def headers_clean_up(headers, to_remove='ContactID'):
+    """
+    Removes an element (or group of elements) from a list.
+
+    Parameters
+    ----------
+    headers
+        A list of column names.
+    to_remove
+        An element (or group of elements) to remove from a list.
+
+    Returns
+    -------
+        A sliced list.
+    """
     return headers.remove(to_remove)
 
 
 def get_user_id(sf, obj_id, att, user_email):
+    """
+    Given an email address, returns a list of metadata to use when uploading to salesforce.
+    Parameters
+    ----------
+    sf
+        An authenticated Salesforce REST API session.
+    obj_id
+        An 18-char string; Represents an Id of a Salesforce object.
+    att
+        A string; Represents the name of an attachment.
+    user_email
+        A string; Represents a given user's email address.
+
+    Returns
+    -------
+    A list of metadata to use when uploading to salesforce
+    """
     import os
     user_results = sf.query(sfdc_object='User', fields=['Email', 'Id'], where="Email='%s'" % user_email)
     user_results = user_results.to_dict(orient='list')
