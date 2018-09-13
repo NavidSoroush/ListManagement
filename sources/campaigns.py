f975797ccca7e879620ac5fffbcd1e3afd953ff8
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-09-13 13:12:10 -0400

    adds docstrings to all modules in /sources/*

--- sources/campaigns.py
+++ sources/campaigns.py
@@ -1,8 +1,35 @@
+"""
+campaigns.py
+======================================
+Contains functions relative to processing
+lists requests sourced from the Campaign
+object in Salesforce.
+"""
+
 from ListManagement.utility import general as ghelp
 from ListManagement.utility import pandas_helper as phelp
 
 
 def parse(path, frame, dict_elements, event_timing):
+    """
+    Function to help parse Campaign list requests into
+    actionable Salesforce jobs (update, create).
+
+    Parameters
+    ----------
+    path
+        String; Represents a full file path to the source list.
+    frame
+        Pandas data frame
+    dict_elements
+        Dictionary; Contains metadata generated during list processing.
+    event_timing
+        String; Denotes whether a campaign has happened or not.
+
+    Returns
+    -------
+        Tuple; updated dictionary and list of files created.
+    """
     if event_timing == 'Post':
         dict_elements['cmp_status'] = 'Needs Follow-Up'
     else:
@@ -24,6 +51,30 @@ def parse(path, frame, dict_elements, event_timing):
 
 
 def make_sc(path, frame, record_name, obj_id, obj):
+    """
+    Function to manufacture a 'source_channel' for contacts
+    that need to be created.
+
+    A source channel gives FS Investments the ability to see
+    from where and when a contact record was sourced from.
+
+    Parameters
+    ----------
+    path
+        String; Represents a full file path.
+    frame
+        Pandas data frame; Object containing relational data.
+    record_name
+        String; Represents the name of a salesforce record.
+    obj_id
+        String; Represents the id of a Salesforce record
+    obj
+        String; Represents the name of a Salesforce object.
+
+    Returns
+    -------
+        Tuple; updated pandas dataframe, move to bulk (boolean), and to_create_path (string)
+    """
     move_to_bulk = False
     sc_to_add = 'conference_' + record_name + '_' + ghelp.yyyy_mm
     if 'to_create_path' in path:
