afbbe1f207af2e4186d10f4ec25b77adee32e2d7
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-10-17 13:52:48 -0400

    update search/ml/header_predictions.py for new list queue methodology  #165

--- search/ml/header_predictions.py
+++ search/ml/header_predictions.py
@@ -12,9 +12,10 @@ def _update_column_names_with_predictions():
     pass
 
 
-def predict_headers_and_pre_processing(path, obj, log, mode):
+def predict_headers_and_pre_processing(_vars, log, mode):
+    _vars.state = _vars.States(_vars.state + 1).value
     model = HeaderPredictions(log=log, use_saved=True)
-    model.predict(predict_path=path, obj=obj)
+    model.predict(predict_path=_vars.list_base_path, obj=_vars.list_type)
     headers = model.p_df.columns.values
     log.info("Here are the headers in the '%s' file: \n\n %s \n" % (model.predict_file_name, headers))
     output = make_df(data={"1. Header": headers, "3. Prediction": model.predictions})
@@ -29,54 +30,53 @@ def predict_headers_and_pre_processing(path, obj, log, mode):
 
     new_headers = []
     for index, row in need_validation.iterrows():
-        if mode != 'auto':
-            if model.probability[index] > _confidence:
-                tmp = [row['Header Value'], row['Class']]
-                new_headers.append(tmp)
-                model.p_df.rename(columns={headers[index]: new_headers[index][1]}, inplace=True)
-            else:
-                log.info("\nHeader given: '%s'"
-                         "\nMy prediction: '%s'"
-                         "\nMy confidence: %s." % (str(row['Header Value']), str(row['Class']),
-                                                   "{0:.0f}%".format(model.probability[index] * 100)))
-                was_i_right = ""
-                while was_i_right.lower not in ('y', 'n'):
-                    was_i_right = input("Was I right? Please just put 'Y' or 'N'.\n")
-                    if was_i_right.lower() == 'y':
-                        tmp = [row['Header Value'], row['Class']]
-                        new_headers.append(tmp)
-                        log.info("Thanks. Updating your file and my training data.\n")
-                        break
-                    elif was_i_right.lower() == 'n':
-                        expected = ""
-                        while expected not in expected_inputs:
-                            log.info("Can you tell me what it should have been?\n")
-                            expected = input("\n".join(expected_inputs) + '\n\n')
-                            if expected in expected_inputs:
-                                tmp = [row['Header Value'], expected]
-                                new_headers.append(tmp)
-                                log.info("Thanks. Updating your file and my training data.\n")
-                                break
-                            else:
-                                log.info("Sorry, I think you typed a value wrong.")
-                        break
-                    else:
-                        log.info("I don't think you typed 'Y' or 'N', can you try again?")
-
-                model.p_df.rename(columns={headers[index]: new_headers[index][1]}, inplace=True)
+        if model.probability[index] > _confidence or mode == 'auto':
+            tmp = [row['Header Value'], row['Class']]
+            new_headers.append(tmp)
+            model.p_df.rename(columns={headers[index]: new_headers[index][1]}, inplace=True)
         else:
+            log.info("\nHeader given: '%s'"
+                     "\nMy prediction: '%s'"
+                     "\nMy confidence: %s." % (str(row['Header Value']), str(row['Class']),
+                                               "{0:.0f}%".format(model.probability[index] * 100)))
+            was_i_right = ""
+            while was_i_right.lower not in ('y', 'n'):
+                was_i_right = input("Was I right? Please just put 'Y' or 'N'.\n")
+                if was_i_right.lower() == 'y':
+                    tmp = [row['Header Value'], row['Class']]
+                    new_headers.append(tmp)
+                    log.info("Thanks. Updating your file and my training data.\n")
+                    break
+                elif was_i_right.lower() == 'n':
+                    expected = ""
+                    while expected not in expected_inputs:
+                        log.info("Can you tell me what it should have been?\n")
+                        expected = input("\n".join(expected_inputs) + '\n\n')
+                        if expected in expected_inputs:
+                            tmp = [row['Header Value'], expected]
+                            new_headers.append(tmp)
+                            log.info("Thanks. Updating your file and my training data.\n")
+                            break
+                        else:
+                            log.info("Sorry, I think you typed a value wrong.")
+                    break
+                else:
+                    log.info("I don't think you typed 'Y' or 'N', can you try again?")
+
             model.p_df.rename(columns={headers[index]: new_headers[index][1]}, inplace=True)
 
-    model.p_df = pre_processing(df=model.p_df, obj=obj)
+    account_name = _vars.account_name if _vars.account_name is not None else _vars.object_name
+    model.p_df = pre_processing(df=model.p_df, obj=account_name)
 
     # clean up this part of the code
     new_data = make_df(data=new_headers, columns=('Header Value', 'Class'))
     num_records = len(model.p_df.index)
     new_brain = concat_dfs([read_df(model.brain), new_data])
     save_df(new_brain, model.brain)
-    save_df(df=model.p_df, path=path)
-
-    return {'Next Step': 'Matching', 'Total Records': num_records, 'Headers': model.p_df.columns.values}
+    _vars.list_df = model.p_df
+    _vars.total_records = num_records
+    _vars.state = _vars.States(_vars.state + 1).value
+    return _vars
 
 
 def pre_processing(df, obj):
