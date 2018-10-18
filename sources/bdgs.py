b22861b5a5a2b1ee37a17926a910aebaf582b218
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-10-18 09:44:01 -0400

    update bdg.py & campaign.py for new list model methodology

--- sources/bdgs.py
+++ sources/bdgs.py
@@ -7,10 +7,9 @@ object in Salesforce.
 """
 
 from ListManagement.utility import general as ghelp
-from ListManagement.utility import pandas_helper as phelp
 
 
-def parse(path, frame, dict_elements):
+def parse(_vars):
     """
     Function to help parse BizDev Group list requests into
     actionable Salesforce jobs (update, create).
@@ -28,31 +27,23 @@ def parse(path, frame, dict_elements):
     -------
         Tuple; updated dictionary and list of files created.
     """
-    dict_elements['no_update_path'] = ghelp.create_path_name(path=path, new_name='no_updates')
-    dict_elements['update_path'] = ghelp.create_path_name(path=path, new_name='to_update')
-    dict_elements['to_create_path'] = ghelp.create_path_name(path=path, new_name='to_create')
-    dict_elements['bdg_update_path'] = ghelp.create_path_name(path=path, new_name='bdg_update')
+    _vars.no_update_path = ghelp.create_path_name(_vars.list_base_path, 'no_updates')
+    _vars.update_path = ghelp.create_path_name(_vars.list_base_path, 'to_update')
+    _vars.create_path = ghelp.create_path_name(_vars.list_base_path, 'to_create')
+    _vars.src_object_upload_path = ghelp.create_path_name(_vars.list_base_path, 'bdg_update')
 
-    no_update_df = frame[(frame['AccountId'].notnull()) & (frame['Needs Info Updated?'] == 'N')]
-    to_update_df = frame[(frame['AccountId'].notnull()) & (frame['Needs Info Updated?'] != 'N')]
-    to_create_df = frame[frame['AccountId'].isnull()]
-    bdg_update_df = frame[(frame['AccountId'].notnull()) & (
-            frame['Licenses'].str.contains('Series 7') | frame['Licenses'].str.contains('Series 22'))]
+    _vars.no_update_df = _vars.found_df[
+        (_vars.found_df['AccountId'].notnull()) & (_vars.found_df['Needs Info Updated?'] == 'N')]
+    _vars.update_df = _vars.found_df[
+        (_vars.found_df['AccountId'].notnull()) & (_vars.found_df['Needs Info Updated?'] != 'N')]
+    _vars.create_df = _vars.found_df[_vars.found_df['AccountId'].isnull()]
+    _vars.src_object_upload_path = _vars.found_df[(_vars.found_df['AccountId'].notnull()) & (
+            _vars.found_df['Licenses'].str.contains('Series 7') | _vars.found_df['Licenses'].str.contains('Series 22'))]
 
-    dict_elements['n_no_update'] = len(no_update_df.index)
-    dict_elements['n_to_update'] = len(to_update_df.index)
-    dict_elements['n_to_create'] = len(to_create_df.index)
-    dict_elements['n_bdg_update'] = len(bdg_update_df.index)
-
-    phelp.save_df(df=no_update_df, path=dict_elements['no_update_path'])
-    phelp.save_df(df=to_update_df, path=dict_elements['update_path'])
-    phelp.save_df(df=to_create_df, path=dict_elements['to_create_path'])
-    phelp.save_df(df=bdg_update_df, path=dict_elements['bdg_update_path'])
-    files_created = ['no_update_path', 'update_path', 'to_create_path', 'bdg_update_path']
-    return dict_elements, files_created
+    return _vars
 
 
-def make_sc(path, frame, record_name, obj_id, obj, aid):
+def make_sc(path, frame, _vars):
     """
     Function to manufacture a 'source_channel' for contacts
     that need to be created.
@@ -79,21 +70,19 @@ def make_sc(path, frame, record_name, obj_id, obj, aid):
     -------
         Tuple; updated pandas dataframe, move to bulk (boolean), and to_create_path (string)
     """
-    sc_to_add = 'bdg_' + record_name + '_' + ghelp.yyyy_mm
-    if 'to_create_path' in path:
-        frame = ghelp.drop_unneeded_columns(frame, obj)
-        to_create = len(frame.index)
-        frame.loc[frame['AccountId'].isnull(), 'AccountId'] = aid
-        frame.loc[frame['SourceChannel'].isnull(), 'SourceChannel'] = sc_to_add
-    elif 'bdg_update_path' in path:
-        frame = ghelp.drop_unneeded_columns(frame, obj, bdg=True)
-        to_create = 0
-        frame['BizDev Group'] = obj_id
-    else:
-        frame = ghelp.drop_unneeded_columns(frame, obj)
-        to_create = 0
-        frame['AccountId'] = aid
-        frame['BizDev Group'] = obj_id
+    if len(frame.index) > 0:
+        sc_to_add = 'bdg_' + _vars.object_name + '_' + ghelp.yyyy_mm
+        if 'to_create_path' in path:
+            frame = ghelp.drop_unneeded_columns(frame,  _vars.list_type)
+            frame.loc[frame['AccountId'].isnull(), 'AccountId'] = _vars.account_id
+            frame.loc[frame['SourceChannel'].isnull(), 'SourceChannel'] = sc_to_add
+        elif 'bdg_update_path' in path:
+            frame = ghelp.drop_unneeded_columns(frame, _vars.list_type, bdg=True)
+            frame['BizDev Group'] = _vars.object_id
+        else:
+            frame = ghelp.drop_unneeded_columns(frame, _vars.list_type)
+            frame['AccountId'] = _vars.account_id
+            frame['BizDev Group'] = _vars.object_id
 
-    move_to_bulk = ghelp.determine_move_to_bulk_processing(frame)
-    return frame, move_to_bulk, to_create
+        _vars.bulk_processing = ghelp.determine_move_to_bulk_processing(frame)
+    return frame, _vars
