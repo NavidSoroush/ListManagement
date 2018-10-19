4f089ad9dfa1b932c04607d6c2559bf8af1f50bb
rickyschoolsFS <ricky.schools@fsinvestments.com>
2018-10-19 16:37:54 -0400

    bump version
    fixes #165

--- __init__.py
+++ __init__.py
@@ -1,9 +1,3 @@
 from __future__ import absolute_import
 
-from .search.finra import Finra
-from .search.salesforce import Search
-from .search.ml.header_predictions import predict_headers_and_pre_processing
-from .utility.email_reader import MailBoxReader
-
-
-__version__ = '4.0a'
+__version__ = '4.1'
